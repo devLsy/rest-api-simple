c2286c3ee2808b8af7e13830a565057d6369a51b
이상엽 <infightersy@naver.com>
2025-04-08 13:34:59 +0900

    수정

--- src/main/java/com/test/lsy/apitest250408/dto/entity/UserEntity.java
+++ src/main/java/com/test/lsy/apitest250408/dto/entity/UserEntity.java
@@ -6,8 +6,6 @@ import lombok.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "user_info2")
 public class UserEntity {
@@ -15,7 +13,6 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String gender;
 
     private String firstName;
@@ -24,4 +21,14 @@ public class UserEntity {
     private String email;
     private String phone;
     private String pictureUrl;
+
+    @Builder
+    public UserEntity(String gender, String firstName, String lastName, String email, String phone, String pictureUrl) {
+        this.gender = gender;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.pictureUrl = pictureUrl;
+    }
 }
